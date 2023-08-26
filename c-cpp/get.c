eed54cde7e4ac5ccd9f2e7190abec73a11e50d84
Joel Abreu <joelabreurojas@gmail.com>
2023-08-26 19:37:42 -0400

    feat(c-cpp): add an auxiliar for inputs

---
+++ c-cpp/get.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_string(char *);
+
+int main(void)
+{
+    char *input = get_string("Input: ");
+    printf("%s", input);
+    free(input);
+
+    return 0;
+}
+
+char *get_string(char *message)
+{
+    int c = 0, len = 0;
+    char *str = NULL, *aux_str = NULL;
+
+    printf("%s", message);
+
+    str = malloc(len + 1);
+    while ((c = getchar()) != '\n') {
+        str[len] = (char)c;
+        len++;
+        aux_str = realloc(str, len + 1);
+        str = aux_str;
+    }
+    str[len] = '\0';
+
+    return str;
+}
