50c7b3a09c3323fc57bfc03415055de126e3719e
Joel Abreu <joelabreurojas@gmail.com>
2023-08-30 19:24:16 -0400

    feat(c-cpp): add a rotate-letter cipher

---
+++ c-cpp/caesar.c
@@ -0,0 +1,46 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "input.h"
+
+enum { LETTERS_RANGE = 26 };
+
+char rotate_letter(char, int);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2 || !isdigit(*argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    
+    char *text = get_string("plaintext:  ");
+    int key = (int)strtol(argv[1], (char **)NULL, 10);
+
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(text); i++)
+        putchar(rotate_letter(text[i], key));
+
+    free(text);
+    putchar('\n');
+
+    return 0;
+}
+
+char rotate_letter(char letter, int gap)
+{
+    if (!islower(letter) && !isupper(letter))
+        return letter;
+
+    char aux = (islower(letter)) ? 'a' : 'A';
+    int size = (int)letter - aux + gap;
+    
+    while (size >= LETTERS_RANGE)
+        size -= LETTERS_RANGE;
+
+
+    return size + aux;
+}
