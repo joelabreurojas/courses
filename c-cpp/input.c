7f3a8d7598b82f9cebd61b97c13a05ad2c63e490
Joel Abreu <joelabreurojas@gmail.com>
2023-09-04 18:46:53 -0400

    refactor(c-cpp): split input logic into header and source files

---
+++ c-cpp/input.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "input.h"
+
+static char *aux = NULL;
+static char *str = NULL;
+static size_t len = 0;
+
+char *get_string(char *message)
+{
+    int c = 0;
+
+    printf("%s", message);
+
+    str = malloc(len + 1);
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+        str[len] = (char)c;
+        len++;
+        aux = realloc(str, len + 1);
+        str = aux;
+    }
+    str[len] = '\0';
+
+    return str;
+}
+
+#if defined (__GNUC__)
+    static void __attribute__((destructor)) teardown();
+#else
+    #error Default compiler / version is not recognized.
+#endif
+
+void teardown(void)
+{
+    free(str);
+}
