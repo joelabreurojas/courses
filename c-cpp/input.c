95fed9c6515bb6b031c078c806544e431973f614
Joel Abreu <joelabreurojas@gmail.com>
2023-09-06 09:15:07 -0400

    feat(c-cpp): add functions to get char, double, float, int and long from user input

--- c-cpp/input.c
+++ c-cpp/input.c
@@ -1,3 +1,6 @@
+#include <float.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +8,105 @@
 #include "input.h"
 
 char *str = NULL;
+
+
+char get_char(char *message)
+{
+    while(true)
+    {
+        char *input = get_string(message);
+        if (!input)
+        {
+            return CHAR_MAX;
+        }
+
+        char c, i;
+        if (sscanf(input, "%c%c", &c, &i) == 1)
+        {
+            return c;
+        }
+    }
+}
+
+double get_double(char *message)
+{
+    while(true)
+    {
+        char *input = get_string(message);
+        if (!input)
+        {
+            return DBL_MAX;
+        }
+
+        char c;
+        double d = 0;
+
+        if (sscanf(input, "%lg%c", &d, &c) == 1)
+        {
+            return d;
+        }
+    }
+}
+
+float get_float(char *message)
+{
+    while(true)
+    {
+        char *input = get_string(message);
+        if (!input)
+        {
+            return FLT_MAX;
+        }
+
+        char c;
+        float f = 0;
+
+        if (sscanf(input, "%g%c", &f, &c) == 1)
+        {
+            return f;
+        }
+    }
+}
+
+int get_int(char *message)
+{
+    while(true)
+    {
+        char *input = get_string(message);
+        if (!input)
+        {
+            return INT_MAX;
+        }
+
+        char c;
+        int i = 0;
+
+        if (sscanf(input, "%i%c", &i, &c) == 1)
+        {
+            return i;
+        }
+    }
+}
+
+long get_long(char *message)
+{
+    while(true)
+    {
+        char *input = get_string(message);
+        if (!input)
+        {
+            return LONG_MAX;
+        }
+
+        char c;
+        long l = 0;
+
+        if (sscanf(input, "%li%c", &l, &c) == 1)
+        {
+            return l;
+        }
+    }
+}
 
 char *get_string(char *message)
 {
--- c-cpp/input.h
+++ c-cpp/input.h
@@ -1,6 +1,11 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+char get_char(char *message);
+double get_double(char *message);
+float get_float(char *message);
+int get_int(char *message);
+long get_long(char *message);
 char *get_string(char *message);
 
 #endif
