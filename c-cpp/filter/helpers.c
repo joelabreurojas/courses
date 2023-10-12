ba72a561ff2ce080e4ef73547d37af025cc39b5b
Joel Abreu <joelabreurojas@gmail.com>
2023-10-11 22:44:02 -0400

    feat(c-cpp/filter): add themes
    - grayscale
    - reflect
    - blur

--- c-cpp/filter/helpers.c
+++ c-cpp/filter/helpers.c
@@ -1,20 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = round(
+                (float)
+                (image[i][j].rgbtRed +
+                image[i][j].rgbtGreen +
+                image[i][j].rgbtBlue) / 3
+            );
+
+            image[i][j].rgbtRed =
+            image[i][j].rgbtGreen =
+            image[i][j].rgbtBlue = average;
+        }
+    }
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    { 
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+    }
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int count = 0, red = 0, green = 0, blue = 0;
+
+            for (int k = i - 1; k <= i + 1; k++)
+            {
+                for (int l = j - 1; l <= j + 1; l++)
+                {
+                    if (k >= 0 && k < height && l >= 0 && l < width)
+                    {
+                        red += image[k][l].rgbtRed;
+                        green += image[k][l].rgbtGreen;
+                        blue += image[k][l].rgbtBlue;
+
+                        count++;
+                    }
+                }
+            }
+
+            image[i][j].rgbtRed = round((float) red / count);
+            image[i][j].rgbtGreen = round((float) green / count);
+            image[i][j].rgbtBlue = round((float) blue / count);
+        }
+    }
+
     return;
 }
 
