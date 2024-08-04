9b28f4a79ebb5931772fc075fb34fdeaa8d419eb
Joel Abreu <joelabreurojas@gmail.com>
2024-08-04 17:25:04 -0400

    feat(functions_variables): add basic i/o exercise

---
+++ python/cs50p/0-functions_variables/indoor.py
@@ -0,0 +1,15 @@
+# Indoor Voice
+
+
+def main():
+    """
+    Prompts the user for input and then outputs that same input in lowercase.
+    Punctuation and whitespace should be outputted unchanged. You’re welcome, but
+    not required, to prompt the user explicitly, as by passing a str of your own as
+    an argument to input.
+    """
+
+    print("Output:", input("Enter an upper-case string: ").lower())
+
+
+main()
