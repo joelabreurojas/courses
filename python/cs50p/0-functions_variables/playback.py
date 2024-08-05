dd80bed3db76b56e98dcd7926722aeffa0e942d7
Joel Abreu <joelabreurojas@gmail.com>
2024-08-05 07:10:57 -0400

    feat(functions_variables): add space replacement
    A string manipulation exercise to take the user input and replace each
    space with `...` (i.e, three periods).

---
+++ python/cs50p/0-functions_variables/playback.py
@@ -0,0 +1,13 @@
+# Playback Speed
+
+
+def main():
+    """
+    Prompts the user for input and then outputs that same input, replacing each
+    space with ... (i.e., three periods).
+    """
+
+    print("Output:", input("Enter a phrase: ").replace(" ", "..."))
+
+
+main()
