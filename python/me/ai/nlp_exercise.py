5b7e05dc72bab216907536b4e178f5d1a86626ee
Joel Abreu Rojas <joelabreurojas@gmail.com>
2025-05-02 16:41:59 -0400

    feat(ai): add nlp exercise script
    This commit adds an NLP exercise script using Spacy. It demonstrates
    text preprocessing, part-of-speech extraction, named entity
    recognition, and relationship extraction from a sample text.

---
+++ python/me/ai/nlp_exercise.py
@@ -0,0 +1,106 @@
+from collections import defaultdict
+
+import spacy
+from spacy.language import Language
+from spacy.tokens.doc import Doc
+
+
+def main() -> None:
+    text: str = "Google Inc. is headquartered in Mountain View, California."
+
+    nlp: Language = spacy.load(name="en_core_web_sm")
+
+    formatted_text: str = preprocess_text(text, nlp)
+
+    doc: Doc = nlp(formatted_text)
+
+    parts_of_speech: dict = extract_pos(doc)
+
+    name_entities: dict = extract_ner(text, nlp)
+
+    relationships: str = extract_relationships(nlp(text))
+
+    display_results(text, doc, parts_of_speech, name_entities, relationships)
+
+
+def preprocess_text(text: str, nlp: Language) -> str:
+    """
+    Formats the given text by removing stop words, punctuation, and spaces using
+    the given spaCy model with lemmatization.
+    """
+
+    doc: Doc = nlp(text)
+
+    tokens: list[str] = [
+        token.lemma_
+        for token in doc
+        if not token.is_stop and not token.is_punct and not token.is_space
+    ]
+
+    return " ".join(tokens)
+
+
+def extract_pos(doc: Doc) -> dict:
+    """
+    Extract the parts of speech from the given spaCy document.
+    """
+
+    parts_of_speech: dict = defaultdict(list)
+
+    for token in doc:
+        parts_of_speech[token.pos_].append(token.text)
+
+    return parts_of_speech
+
+
+def extract_ner(text: str, nlp: Language) -> dict:
+    """
+    Extracts the named entities from the given text using the given the spaCy model.
+    """
+
+    doc: Doc = nlp(text)
+
+    named_entities: dict = defaultdict(list)
+
+    for ent in doc.ents:
+        named_entities[ent.label_].append(ent.text)
+
+    return named_entities
+
+
+def extract_relationships(doc: Doc) -> str:
+    """
+    Extracts the relationships between tokens in the given spaCy document.
+    """
+
+    relationships: list = [token.text for token in doc]
+
+    for index, token in enumerate(doc):
+        if token.dep_ != "compound":
+            relationships[index] += " -"
+
+    sentence: str = " ".join(relationships)
+
+    # Remove suffix if the last token isn't compound
+    return sentence.removesuffix(" -")
+
+
+def display_results(
+    text: str, doc: Doc, parts_of_speech: dict, name_entities: dict, relationships: str
+):
+    print("Original Text:", text)
+    print("Preprocessed Text:", doc)
+
+    print("Parts of Speech:")
+    for pos, tokens in parts_of_speech.items():
+        print(f"  {pos}: {', '.join(tokens)}")
+
+    print("Named Entities:")
+    for ent, tokens in name_entities.items():
+        print(f"  {ent}: {', '.join(tokens)}")
+
+    print("Relationships:", relationships)
+
+
+if __name__ == "__main__":
+    main()
